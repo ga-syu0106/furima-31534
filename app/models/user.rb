bcf8c6e34b21127f116b47f774825f859a1c0b4c
ga-syu0106 <s.01vanishment06@gmail.com>
2021-01-16 16:41:05 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :nickname        , presence: true
-  validates :password        , presence: true , format: { with: PASSWORD_REGEX , message: "英数字のみで両方使用してください"}
+  validates :encrypted_password        , presence: true , format: { with: PASSWORD_REGEX , message: "英数字のみで両方使用してください"}
   validates :last_name       , presence: true , format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角文字を使用してください' }
   validates :first_name      , presence: true , format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角文字を使用してください' }
   validates :last_name_kana  , presence: true , format: { with: /\A[ァ-ヶ]+\z/ , message: '全角カナを使用してください' }
