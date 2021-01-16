8e2c68d3470c0e5002e7768ddd1a1a835d170824
ga-syu0106 <s.01vanishment06@gmail.com>
2021-01-16 14:45:05 +0900

    パスワード　バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :nickname        , presence: true
-  validates :password        , presence: true , format: { with: /\A[a-zA-Z0-9]+[a-zA-Z0-9]\z/ , message: "英数字のみで両方使用してください"}
+  validates :password        , presence: true , format: { with: PASSWORD_REGEX , message: "英数字のみで両方使用してください"}
   validates :last_name       , presence: true , format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角文字を使用してください' }
   validates :first_name      , presence: true , format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角文字を使用してください' }
   validates :last_name_kana  , presence: true , format: { with: /\A[ァ-ヶ]+\z/ , message: '全角カナを使用してください' }
