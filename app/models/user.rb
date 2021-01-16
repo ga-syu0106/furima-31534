43a654fa448a54f75d0f784fec7a41f26dd9cc56
ga-syu0106 <s.01vanishment06@gmail.com>
2021-01-17 00:50:57 +0900

    バリデーション　リファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :nickname                  , presence: true
-  validates :password                  , presence: true , format: { with: PASSWORD_REGEX , message: "英数字のみで両方使用してください"}
-  validates :last_name                 , presence: true , format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角文字を使用してください' }
-  validates :first_name                , presence: true , format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角文字を使用してください' }
-  validates :last_name_kana            , presence: true , format: { with: /\A[ァ-ヶ]+\z/ , message: '全角カナを使用してください' }
-  validates :first_name_kana           , presence: true , format: { with: /\A[ァ-ヶ]+\z/ , message: '全角カナを使用してください' }
-  validates :birth_date                , presence: true
+  with_options presence: true do
+  validates :nickname
+  validates :password                   , format: { with: PASSWORD_REGEX , message: "半角英数字のみで両方使用してください"}
+  validates :last_name                  , format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角文字を使用してください' }
+  validates :first_name                 , format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角文字を使用してください' }
+  validates :last_name_kana             , format: { with: /\A[ァ-ヶ]+\z/ , message: '全角カナを使用してください' }
+  validates :first_name_kana            , format: { with: /\A[ァ-ヶ]+\z/ , message: '全角カナを使用してください' }
+  validates :birth_date
+  end
 end
