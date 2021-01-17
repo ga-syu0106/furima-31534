83434c655c37b6045a14640cbad7a667c7ddc34a
ga-syu0106 <s.01vanishment06@gmail.com>
2021-01-17 23:18:58 +0900

    rubocou -a 実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   with_options presence: true do
-  validates :nickname
-  validates :password                   , format: { with: PASSWORD_REGEX , message: "半角英数字のみで両方使用してください"}
-  validates :last_name                  , format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角文字を使用してください' }
-  validates :first_name                 , format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角文字を使用してください' }
-  validates :last_name_kana             , format: { with: /\A[ァ-ヶ]+\z/ , message: '全角カナを使用してください' }
-  validates :first_name_kana            , format: { with: /\A[ァ-ヶ]+\z/ , message: '全角カナを使用してください' }
-  validates :birth_date
+    validates :nickname
+    validates :password, format: { with: PASSWORD_REGEX, message: '半角英数字のみで両方使用してください' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナを使用してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナを使用してください' }
+    validates :birth_date
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :user do
     transient do
-      person{ Gimei.name}
+      person { Gimei.name }
     end
-    nickname              {Faker::Name}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 20)}
-    password_confirmation {password}
-    last_name             {person.last.kanji}
-    first_name            {person.first.kanji}
-    last_name_kana        {person.last.katakana}
-    first_name_kana       {person.first.katakana}
-    birth_date            {Faker::Date.between(from: '1930-01-01', to: 5.years.ago)}
+    nickname              { Faker::Name }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 20) }
+    password_confirmation { password }
+    last_name             { person.last.kanji }
+    first_name            { person.first.kanji }
+    last_name_kana        { person.last.katakana }
+    first_name_kana       { person.first.katakana }
+    birth_date            { Faker::Date.between(from: '1930-01-01', to: 5.years.ago) }
   end
 end
