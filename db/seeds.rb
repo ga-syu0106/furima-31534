3baf771e4e69e3b4ddcae080b82e2a61c1ad5160
ga-syu0106 <s.01vanishment06@gmail.com>
2021-01-22 00:11:24 +0900

    seed delivery_free 追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@ if State.all.length == 0
   states = [{state_name:"新品、未使用"},{state_name:"未使用に近い"},{state_name:"目立った傷や汚れなし"},{state_name:"やや傷や汚れあり"},{state_name:"傷や汚れあり"},{state_name:"全体的に状態が悪い"},]
   State.import states
 end
+
+if DeliveryFree.all.length == 0
+  delivery_frees = [{delivery_free_name:"着払い(購入者負担)"},{delivery_free_name:"送料込み(出品者負担)"}]
+  DeliveryFree.import delivery_frees
+end
