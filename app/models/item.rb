039cb39036d60f8d1f63eebe62a06cf05a55bf38
ga-syu0106 <s.01vanishment06@gmail.com>
2021-01-23 12:36:06 +0900

    delivery_free　アクティブハッシュ作成　アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :state
+  belongs_to :delivery_free
 
   validates :category_id , presence: true
 end
