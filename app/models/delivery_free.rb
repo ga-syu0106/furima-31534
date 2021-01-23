ec78cc5e83e0957bbe021026fb84efd43a1d18d7
ga-syu0106 <s.01vanishment06@gmail.com>
2021-01-23 11:40:23 +0900

    選択をアクティブハッシュに変更するために追加モデル・seedの中身の削除

--- app/models/delivery_free.rb
+++
@@ -1,2 +0,0 @@
-class DeliveryFree < ApplicationRecord
-end
--- db/migrate/20210121145820_create_delivery_frees.rb
+++
@@ -1,8 +0,0 @@
-class CreateDeliveryFrees < ActiveRecord::Migration[6.0]
-  def change
-    create_table :delivery_frees do |t|
-      t.string :delivery_free_name , null: false
-      t.timestamps
-    end
-  end
-end
