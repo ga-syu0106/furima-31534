8e6413ebd99fd3f4bae45a9fe4658532293bd13a
ga-syu0106 <s.01vanishment06@gmail.com>
2021-01-23 16:27:22 +0900

    エラーメッセージの表示　投稿できない時の

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :state_id
+    validates :delivery_free_id
+    validates :prefecture_id
+    validates :days_ship_id
+    validates :price , numericality:{greater_than: 300 , less_than:9999999 ,only_integer: true ,message:"販売価格は半角数字で300円〜9,999,999円の間で入力ください"}
+    validates :image
+  end
+
   has_one_attached :image
   
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -7,6 +19,4 @@ class Item < ApplicationRecord
   belongs_to :delivery_free
   belongs_to :prefecture
   belongs_to :days_ship
-
-  validates :category_id , presence: true
 end
