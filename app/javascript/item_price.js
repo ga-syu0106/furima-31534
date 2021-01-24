56c1d83a6f3d084325928989acd8206382053486
ga-syu0106 <s.01vanishment06@gmail.com>
2021-01-25 00:23:48 +0900

    JS　正規表現の追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,8 +4,13 @@ function price() {
     const  priceVal = itemPrice.value
     const addTaxPrice = document.getElementById("add-tax-price")
     const profit = document.getElementById("profit")
+    if (priceVal.match(/^[0-9]+$/)){
     addTaxPrice.innerHTML = Math.floor(priceVal * 0.1)
     profit.innerHTML = Math.ceil(priceVal - (priceVal * 0.1))
+    } else{
+      addTaxPrice.innerHTML = ""
+      profit.innerHTML = ""
+    }
   })
 }
 window.addEventListener('load' , price)
