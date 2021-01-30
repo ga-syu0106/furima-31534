1997f04f6c708bff8190977687436f5054f068ee
ga-syu0106 <s.01vanishment06@gmail.com>
2021-01-30 21:43:08 +0900

    authenticate アクション名修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :shoe]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_item, only: [:show, :edit, :update, :destroy]
   before_action :check_user, only: [:edit, :destroy, :update]
   def index
