af523b3c181743d4fd4bf71b5908b6ab6ecf3272
ga-syu0106 <s.01vanishment06@gmail.com>
2021-02-09 23:55:44 +0900

    tradeモデル　マイグレート

--- db/migrate/20210209145105_create_trades.rb
+++ db/migrate/20210209145105_create_trades.rb
@@ -1,7 +1,8 @@
 class CreateTrades < ActiveRecord::Migration[6.0]
   def change
     create_table :trades do |t|
-
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
       t.timestamps
     end
   end
