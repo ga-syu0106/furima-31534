2cf8229dd97f4295deb4b43a96319f2394a9ab21
ga-syu0106 <s.01vanishment06@gmail.com>
2021-02-13 19:04:48 +0900

    tradeコントローラー　リファクタリング

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -7,12 +7,7 @@ class TradesController < ApplicationController
   def create
     @trade_address = TradeAddress.new(address_params)
     if @trade_address.valid?
-      Payjp.api_key = "sk_test_f297eefec66ae5a559f134ff"
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: address_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @trade_address.save
       redirect_to root_path
     else
@@ -28,5 +23,14 @@ class TradesController < ApplicationController
 
   def address_params
     params.permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :item_id).merge(token: params[:token], user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_f297eefec66ae5a559f134ff"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: address_params[:token],
+      currency: 'jpy'
+    )
   end
 end
