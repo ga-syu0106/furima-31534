15e6cc67a9e84a75a0180f1051eb43d42b3664a1
ga-syu0106 <s.01vanishment06@gmail.com>
2021-02-17 00:30:07 +0900

    エラー文表示　view

--- app/models/trade_address.rb
+++ app/models/trade_address.rb
@@ -1,7 +1,6 @@
 class TradeAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :token, :user_id, :item_id, :trade_id
-
   with_options presence: true do
     validates :postal_code ,format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "半角数字のみで (-) も入力してください"}
     validates :prefecture_id
@@ -9,10 +8,12 @@ class TradeAddress
     validates :address
     validates :building_name
     validates :phone_number, numericality: {only_integer: true, message:"数字のみで入力してください"}
+
   end
 
   def save
     trade = Trade.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, trade_id: trade.id)
   end
+
 end
